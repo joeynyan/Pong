5d26fa44d8e2d2a448a8943f6d0d8faef2d6cd70
Jo <joeyjot@gmail.com>
2016-08-30 00:28:39 -0700

    Adding a README

--- jFrameTest.java
+++ jFrameTest.java
@@ -5,8 +5,10 @@
 	
 	class jFrameTest extends JFrame {
 	  public jFrameTest() {
+	  	int width = 500;
+	  	int height = 300;
 		setTitle("My Empty Frame");
-		setSize(300,200); // default size is 0,0
+		setSize(width,height); // default size is 0,0
 		setLocation(10,200); // default is 0,0 (top left corner)
 	  }
 	
